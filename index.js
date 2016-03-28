d62ce85ba11efe8b29e97cd92a7bbaa72bda5964
Kyle Warren <qarren@gmail.com>
2016-03-28 15:16:08 -0700

    made default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 
-export function classify() {
+export default function classify() {
   return _(arguments).map(arg => {
     if (_.isPlainObject(arg)) {
       arg = _.map(arg, (test, name) => (
